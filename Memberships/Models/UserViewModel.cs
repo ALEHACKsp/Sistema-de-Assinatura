d639acccbe1f34323c1180a509ce1e012ad70d8d
cesar augusto gadelha <cesar_ags@outlook.com>
2019-03-15 15:06:14 -0300

    Criado user identity e subscriptions for users

---
+++ Memberships/Models/UserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserViewModel
+    {
+        [Display(Name ="User ID")]
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+
+        [Required]
+        [Display(Name ="First name")]
+        [StringLength(30,ErrorMessage =" O {0} não pode ultrapassar o maximo de {1} caracteres")]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+    }
+}
